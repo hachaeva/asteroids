b1d2c9b2ffcd989790b4294baafed21831bdbbac
hachaeva <hachaeva@hotmail.com>
2025-07-05 10:05:54 -0400

    added bullets

--- main.py
+++ main.py
@@ -1,9 +1,9 @@
-import sys
 import pygame
 from constants import *
 from player import Player
 from asteroid import Asteroid
 from asteroidfield import AsteroidField
+from bullets import Shot
 
 import os
 os.environ["SDL_VIDEODRIVER"]="x11"
@@ -18,10 +18,12 @@ def main():
     updatable = pygame.sprite.Group()
     drawable = pygame.sprite.Group()
     asteroids =  pygame.sprite.Group()
+    shots = pygame.sprite.Group()
 
     Player.containers = (updatable, drawable)
     Asteroid.containers = (asteroids,updatable,drawable)
     AsteroidField.containers = (updatable)
+    Shot.containers = (shots,updatable,drawable)
 
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
     clock = pygame.time.Clock()
@@ -44,7 +46,7 @@ def main():
         for asteroid in asteroids:
             if asteroid.collision_detection(player):
                 print("Game over!")
-                sys.exit()
+                raise SystemExit
                
 if __name__ == "__main__":
     main()
--- player.py
+++ player.py
@@ -1,5 +1,6 @@
 import pygame
 from circleshape import CircleShape
+from bullets import Shot
 from constants import *
 
 class Player(CircleShape):
@@ -18,13 +19,6 @@ class Player(CircleShape):
     def draw(self,screen):
         pygame.draw.polygon(screen,"white",self.triangle(),2)
 
-    def rotate(self,dt):
-        self.rotation += PLAYER_TURN_SPEED * dt
-
-    def move(self,dt):
-        forward = pygame.Vector2(0, 1).rotate(self.rotation)
-        self.position += forward * PLAYER_SPEED * dt
-        
     def update(self, dt):
         keys = pygame.key.get_pressed()
 
@@ -36,3 +30,17 @@ class Player(CircleShape):
             self.move(dt)
         if keys[pygame.K_s]:
             self.move(-1*dt)
+        if keys[pygame.K_SPACE]:
+            self.shoot()      
+
+    def rotate(self,dt):
+        self.rotation += PLAYER_TURN_SPEED * dt
+
+    def move(self,dt):
+        forward = pygame.Vector2(0, 1).rotate(self.rotation)
+        self.position += forward * PLAYER_SPEED * dt
+        
+    def shoot(self):
+        bullet = Shot(self.position.x,self.position.y)
+        bullet.velocity = pygame.Vector2(0,1).rotate(self.rotation)*PLAYER_SHOOT_SPEED
+        
