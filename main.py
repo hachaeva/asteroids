cf2cc8725495fb154a7df0aca9014f6a554d947f
hachaeva <hachaeva@hotmail.com>
2025-07-05 00:02:13 -0400

    player rotation

--- main.py
+++ main.py
@@ -18,15 +18,15 @@ def main():
     player = Player(SCREEN_WIDTH/2,SCREEN_HEIGHT/2)
 
     while True:
+        dt=clock.tick(60)/1000
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
         screen.fill("black")
+        player.update(dt)
         player.draw(screen)
         pygame.display.update()
-        dt=clock.tick(60)/1000
+       
         
-
-
 if __name__ == "__main__":
     main()
