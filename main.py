563964691189fe519e29c471832eeacffff1e09e
hachaeva <hachaeva@hotmail.com>
2025-07-04 23:53:52 -0400

    draw player

--- main.py
+++ main.py
@@ -1,8 +1,10 @@
 import pygame
 from constants import *
+from circleshape import *
+from player import *
 import os
 os.environ["SDL_VIDEODRIVER"]="x11"
-os.environ['SDL_AUDIODRIVER'] = 'dsp'
+os.environ['SDL_AUDIODRIVER']='dsp'
 
 def main():
     pygame.init()
@@ -11,13 +13,19 @@ def main():
     print(f"Screen height: {SCREEN_HEIGHT}")
 
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
-    
+    clock = pygame.time.Clock()
+    dt = 0
+    player = Player(SCREEN_WIDTH/2,SCREEN_HEIGHT/2)
+
     while True:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
         screen.fill("black")
+        player.draw(screen)
         pygame.display.update()
+        dt=clock.tick(60)/1000
+        
 
 
 if __name__ == "__main__":
